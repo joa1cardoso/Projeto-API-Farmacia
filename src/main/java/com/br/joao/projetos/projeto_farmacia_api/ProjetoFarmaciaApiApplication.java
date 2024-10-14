bd211b5ecaebb6cddd0e88c564ac2f9758858b0d
João Marcos <joaooliveiracardoso@hotmail.com>
2024-10-14 15:37:52 -0300

    projeto api farmacia

---
+++ src/main/java/com/br/joao/projetos/projeto_farmacia_api/ProjetoFarmaciaApiApplication.java
@@ -0,0 +1,14 @@
+package com.br.joao.projetos.projeto_farmacia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFarmaciaApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetoFarmaciaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/joao/projetos/projeto_farmacia_api/ProjetoFarmaciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.joao.projetos.projeto_farmacia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFarmaciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
